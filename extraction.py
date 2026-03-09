3b1f497d6f39eb3b7d6c0887163ad4a10c970184
Bea Healy <beahealy22@gmail.com>
2026-03-09 17:51:01 +0000

    Add extra flags/passes

--- extraction.py
+++ extraction.py
@@ -274,7 +274,7 @@ def main():
 
             initial_mlir = test_dir + "1_initial.mlir"
             proc_mlir = test_dir + "2_proc.mlir"
-            cmd = [str(CIRCT_OPT), "--hw-flatten-modules", "--hw-flatten-io", "--comb-assume-two-valued", "--arc-strip-sv=async-resets-as-sync", "--lower-ltl-to-core", "--canonicalize", str(initial_mlir), "-o",  str(proc_mlir)]
+            cmd = [str(CIRCT_OPT), "--hw-flatten-modules", "--hw-flatten-io", "--comb-assume-two-valued", "--arc-strip-sv=async-resets-as-sync", "--lower-ltl-to-core='assume-first-clock'", "--canonicalize", "--lower-seq-shiftreg", "--cse", "--canonicalize", str(initial_mlir), "-o",  str(proc_mlir)]
             res = run_command(cmd, cwd=OPENTITAN_ROOT)
             # Hack until FSMToSMT handles clocked asserts
             run_command(["sed", "-i", "-E", "-r", "\"s/clocked_assert (%[a-zA-Z0-9_]+), .* : (.*)/assert \\1 : \\2/g\"", str(proc_mlir)])
